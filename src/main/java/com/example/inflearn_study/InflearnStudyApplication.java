0809a4af7d087b8d2148a7603915c1e3380e72e4
DESKTOP-F0TRJ64\doob9 <hwangis941126@gmail.com>
2024-06-14 21:16:32 +0900

    spinrgboot 스켈레톤

---
+++ src/main/java/com/example/inflearn_study/InflearnStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.inflearn_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/inflearn_study/InflearnStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inflearn_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
